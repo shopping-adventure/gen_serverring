fcbc25074f3431f596f018a76c355534459a73b8
Laurent Picouleau <laurent@kbrwadventure>
2016-07-19 11:36:30 +0200

    specific callback for monitoring ct test

--- test/monitor_test.exs
+++ test/monitor_test.exs
@@ -1,13 +1,15 @@
 defmodule MonitorTest do
   use ExUnit.Case, async: true
 
-  setup do
+  setup context do
     case File.dir?("./data") do
       true -> File.rm_rf("./data")
       false -> :ok
     end
     assert :ok == File.mkdir("./data")
     Application.start(:crdtex)
+    Application.put_env(:gen_serverring, :name, context.name)
+    Application.put_env(:gen_serverring, :callback, context.callback)
     Application.start(:gen_serverring)
 
     on_exit fn() ->
@@ -18,34 +20,51 @@ defmodule MonitorTest do
     :ok
   end
 
-  test "monitoring the default ring" do
-    assert length(GenServerring.all(:demo_ring)) == 1
-    assert length(GenServerring.up(:demo_ring)) == 1
-    :timer.sleep(13_000) # wait to be part of the ring
-    all = GenServerring.all(:demo_ring)
-    up = GenServerring.up(:demo_ring)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(1000)
-    :ct.pal(:debug, 50, 'up is ~p~n', [GenServerring.up(:demo_ring)])
-    :timer.sleep(5_000) # n4 added and enough gossips should have occured
-    all2 = GenServerring.all(:demo_ring)
-    up2 = GenServerring.up(:demo_ring)
-    assert length(all2 -- all) == 1
-    assert length(up2 -- up) == 1
-    :timer.sleep(6_000) # n4 has crashed
-    ^all2 = Genserverring.all(:demo_ring)
-    ^up = Genserverring.up(:demo_ring)
+  def init([]), do: Demo.init([])
+
+  def handle_call(msg, from, state) do
+    :ct.log(:info, 75, 'handle_call ~p~n', [msg], [])
+    Demo.handle_call(msg, from, state)
+  end
+
+  def handle_cast(msg, state) do
+    :ct.log(:info, 75, 'handle_cast ~p~n', [msg], [])
+    Demo.handle_cast(msg, state)
+  end
+
+  def handle_info(msg, state) do
+    :ct.log(:info, 75, 'handle_info ~p~n', [msg], [])
+    {:noreply, state}
+  end
+
+  def handle_state_change(change) do
+    :ct.log(:info, 75, 'handle_state ~p~n', [change], [])
+    :ok
+  end
+
+  def handle_ring_change(change) do
+    :ct.log(:info, 75, 'handle_ring ~p~n', [change], [])
+    :ok
+  end
+
+  @tag name: :ct_test_ring
+  @tag callback: __MODULE__
+  test "monitoring ct_test_ring", context do
+    ring = context.name
+    assert length(GenServerring.all(ring)) == 1
+    assert length(GenServerring.up(ring)) == 1
+    :ct.sleep(13_000) # wait to be part of the ring
+    all = GenServerring.all(ring)
+    up = GenServerring.up(ring)
+    assert length(all) == 3
+    assert length(up) == 3
+    :ct.sleep(5_000) # n4 added and enough gossips should have occured
+    all2 = GenServerring.all(ring)
+    up2 = GenServerring.up(ring)
+    assert length(all2) == 4
+    assert length(up2) == 4
+    :ct.sleep(6_000) # n4 has crashed
+    ^all2 = GenServerring.all(ring)
+    assert length(GenServerring.up(ring)) == 3
   end
 end
